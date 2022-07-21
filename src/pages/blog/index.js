a9849378edd289b85a6787ef06b2d8264a991b97
Mitch Hankins <mitchhankins@icloud.com>
2022-07-21 09:47:06 -0700

    rss

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -11,7 +11,6 @@ import YouTube from "../../img/social/youtube.svg";
 import BlogRoll from "../../components/BlogRoll";
 import Hero from "../../components/Hero";
 import { graphql } from "gatsby";
-import { getImage } from "gatsby-plugin-image";
 
 function encode(data) {
   return Object.keys(data)
@@ -19,8 +18,8 @@ function encode(data) {
     .join("&");
 }
 const Index = ({ data }) => {
-  console.log("data", data);
   const [loading, setLoading] = React.useState(false);
+  const [response, setResponse] = React.useState("");
   const [formState, setFormState] = React.useState({ isValidated: false });
 
   const handleChange = (e) => {
@@ -40,8 +39,21 @@ const Index = ({ data }) => {
         ...formState,
       }),
     })
-      .then(() => setLoading(false))
-      .catch(() => setLoading(false));
+      .then((data) => {
+        setLoading(false);
+        if (data.status === 400) {
+          setResponse("You are already subscribed.");
+          alert("You are already subscribed.");
+        } else {
+          setResponse("Please check your email to confirm your subscription!");
+          alert("Please check your email to confirm your subscription!");
+        }
+      })
+      .catch(() => {
+        setLoading(false);
+        setResponse("Error, something went wrong.");
+        alert("Error, something went wrong.");
+      });
   };
   return (
     <Layout>
@@ -119,63 +131,67 @@ const Index = ({ data }) => {
             </header>
             <div className="card-content blog-card-content">
               <div className="content">
-                <form
-                  name="email"
-                  method="post"
-                  data-netlify="true"
-                  data-netlify-honeypot="bot-field"
-                  onSubmit={handleSubmit}
-                >
-                  {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-                  <input type="hidden" name="form-name" value="email" />
-                  <div hidden>
-                    <label>
-                      Don’t fill this out:{" "}
-                      <input name="bot-field" onChange={handleChange} />
-                    </label>
-                  </div>
-                  <div className="field">
-                    <label className="label" htmlFor={"name"}>
-                      Name:
-                    </label>
-                    <div className="control">
-                      <input
-                        disabled={loading}
-                        className="input"
-                        type={"text"}
-                        name={"name"}
-                        onChange={handleChange}
-                        id={"name"}
-                        required={true}
-                      />
+                {response ? (
+                  response
+                ) : (
+                  <form
+                    name="email"
+                    method="post"
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                    onSubmit={handleSubmit}
+                  >
+                    {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                    <input type="hidden" name="form-name" value="email" />
+                    <div hidden>
+                      <label>
+                        Don’t fill this out:{" "}
+                        <input name="bot-field" onChange={handleChange} />
+                      </label>
                     </div>
-                  </div>
-                  <div className="field">
-                    <label className="label" htmlFor={"email"}>
-                      Email:
-                    </label>
-                    <div className="control">
-                      <input
-                        disabled={loading}
-                        className="input"
-                        type={"email"}
-                        name={"email"}
-                        onChange={handleChange}
-                        id={"email"}
-                        required={true}
-                      />
+                    <div className="field">
+                      <label className="label" htmlFor={"name"}>
+                        Name:
+                      </label>
+                      <div className="control">
+                        <input
+                          disabled={loading}
+                          className="input"
+                          type={"text"}
+                          name={"name"}
+                          onChange={handleChange}
+                          id={"name"}
+                          required={true}
+                        />
+                      </div>
                     </div>
-                  </div>
-                  <div className="field">
-                    <button
-                      disabled={loading}
-                      className="button is-link email-button"
-                      type="submit"
-                    >
-                      {loading ? "Subscribing" : "Sign Up"}
-                    </button>
-                  </div>
-                </form>
+                    <div className="field">
+                      <label className="label" htmlFor={"email"}>
+                        Email:
+                      </label>
+                      <div className="control">
+                        <input
+                          disabled={loading}
+                          className="input"
+                          type={"email"}
+                          name={"email"}
+                          onChange={handleChange}
+                          id={"email"}
+                          required={true}
+                        />
+                      </div>
+                    </div>
+                    <div className="field">
+                      <button
+                        disabled={loading}
+                        className="button is-link email-button"
+                        type="submit"
+                      >
+                        {loading ? "Subscribing" : "Sign Up"}
+                      </button>
+                    </div>
+                  </form>
+                )}
               </div>
             </div>
           </div>
