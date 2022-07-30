13af027088ca7d99a5603dbb334876b1f43dda06
Mitch Hankins <mitchhankins@icloud.com>
2022-07-29 23:12:08 -0700

    hamburger

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,23 @@ const Navbar = () => {
   const [active, setActive] = React.useState(false);
   const [navBarActiveClass, setNavBarActiveClass] = React.useState("");
 
+  React.useEffect(() => {
+    const navbarBurgers = Array.prototype.slice.call(
+      document.querySelectorAll(".navbar-burger"),
+      0
+    );
+
+    navbarBurgers.forEach((el) => {
+      el.addEventListener("click", () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }, []);
+
   function toggleHamburger() {
     setActive(!active);
     setNavBarActiveClass(active ? "is-active" : "");
